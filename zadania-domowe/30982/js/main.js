1b01fea8c111bbcefb7c566ccad25844ef48cd3a
Pusiaczek <lipek8@gmail.com>
2021-06-26 11:01:06 +0200

    rozwiazanie zadania 30982

--- zadania-domowe/30982/js/main.js
+++ zadania-domowe/30982/js/main.js
@@ -1,80 +1,40 @@
-class Pracownik {
-    constructor(id, nazwa, godziny, stawka) {
-        this.id = id;
-        this.nazwa = nazwa;
-        this.godziny = godziny;
-        this.stawka = stawka;
-    }
-
-    get wyplata() {
-        return this.liczWyplate();
-    }
-
-    liczWyplate() {
-        return( (this.godziny > 160) ? (( 160 * this.stawka ) + (this.godziny - 160) * 2 * this.stawka) : this.godziny * this.stawka);
-    }
-}
-
-document.querySelectorAll("input").forEach( input => {
-    input.onchange = (e) => { 
-        console.log(e.target);
-    }
-})
-
-// console.log(document.querySelectorAll("input"));
-
-
-
-
-let pracownicy = [];
-
-document.querySelectorAll("div[id^='pracownik']").forEach( (item, index) => {
-    const nowyPracownik = new Pracownik(index, item.children[0].textContent, item.children[1].value, item.children[2].value)
-    pracownicy.push(nowyPracownik);
-})
-
-// const test = new Pracownik(1, "Fifi", 162, 2)
-// console.log(pracownicy);
-
-
-
-
 document.getElementById("oblicz").onclick = () => {
-    pracownicy.map( (pracownik) => {
-        console.log(pracownik);
-        [...document.querySelectorAll(".wyplata")][pracownik.id].innerHTML = pracownik.wyplata
+    let pracownicyHtml = document.querySelectorAll("div[id^='pracownik']");
+    let pracownicy = [];
+
+    pracownicyHtml.forEach( (pracownikHtml, index) => {
+        pracownicy[index] = {
+            nazwa: pracownikHtml.children[0].textContent,
+            czasPracy: pracownikHtml.children[1].value,
+            stawkaGodzinowa: pracownikHtml.children[2].value,
+            wyplata: 0,
+        }
+
+        if (pracownicy[index].czasPracy > 160) {
+            pracownicy[index].wyplata = ( 160 * pracownicy[index].stawkaGodzinowa ) + (pracownicy[index].czasPracy - 160) * 2 * pracownicy[index].stawkaGodzinowa;
+        }else{
+            pracownicy[index].wyplata = pracownicy[index].stawkaGodzinowa * pracownicy[index].czasPracy;
+        }
+        pracownikHtml.children[3].textContent = pracownicy[index].wyplata;
+        
+
+        if (pracownicy[index].czasPracy < 100) {
+            pracownikHtml.style.backgroundColor = "red";
+        }else{
+            pracownikHtml.style.backgroundColor = "limegreen";
+        }
     })
-    
-    
-    // console.log();
-    // document.querySelectorAll(".wyplata").forEach( (span, index) => {
-        // console.log(span);
-        // span.textContent = 
-    // })
 
 
+    pracownicy.sort( (a, b) => {
+        if (Number(a.czasPracy) < Number(b.czasPracy)) {
+            return 1;
+        }else if (Number(a.czasPracy) > Number(b.czasPracy)) {
+            return -1;
+        }else{
+            return 0;
+        }
+    })
 
-
-    // let pracownicy = document.querySelectorAll("div[id^='pracownik']");
-    // pracownicy.forEach( item => {
-    //     const godziny = item.children[1].value;
-    //     const stawka = item.children[2].value;
-    //     let zarobki = godziny * stawka;
-
-    //     if (godziny > 160) {
-    //         zarobki = ( 160 * stawka ) + (godziny - 160) * 2 * stawka;
-    //     }
-    //     item.children[3].textContent = zarobki;
-
-    //     if (godziny < 100) {
-    //         item.style.backgroundColor = "red";
-    //     }else{
-    //         item.style.backgroundColor = "limegreen";
-    //     }
-    // })
-
-    // console.log(pracownicy[1].children[1].value);
-
+    document.getElementById('najlepsi-pracownicy').textContent = `${pracownicy[0].nazwa}, ${pracownicy[1].nazwa}, ${pracownicy[2].nazwa}!`
 }
-
-
