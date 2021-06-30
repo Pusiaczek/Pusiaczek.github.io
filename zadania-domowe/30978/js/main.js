d6fee5376a386ae22fd2a769000e32369c137eb7
Pusiaczek <lipek8@gmail.com>
2021-06-30 16:44:48 +0200

    Poprawki do zadania 30978

--- zadania-domowe/30978/js/main.js
+++ zadania-domowe/30978/js/main.js
@@ -1,7 +1,10 @@
 $( () => {
     $('button').click( () => {
         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', (data) => {
-    $('#dane-programisty').text(JSON.stringify(data))
+    $('#dane-programisty').html(`Imię: ${data.imie} <br>
+                                Nazwisko: ${data.nazwisko} <br>
+                                Zawód: ${data.zawod} <br>
+                                Firma: ${data.firma}`)
         })
     })
 })
